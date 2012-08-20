cafc28d16d66d9bb1baf723931fcf04815ccd5a6
Tom Marshall <tdm.code@gmail.com>
2012-08-20 18:06:30 -0500

    Convert to new build layout.
    Change-Id: I2486c4b8132b5e758f189ec0ace75970ca61b43e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,14 @@
 include vendor/lge/VS660/BoardConfigVendor.mk
 
 # Ref: http://pastebin.com/UyppT859
+# XXX: Actual recovery partition is 0x00500000.
+# This is too small and causes the build to fail.
+# So we lie here.  DO NOT FLASH THIS RECOVERY!
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x00500000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00700000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0bb00000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0b8e0000
 
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/lge/thunderc
-TARGET_KERNEL_CONFIG := thunderc-VS660_defconfig
-#TARGET_PREBUILT_KERNEL := device/lge/VS660/kernel/zImage
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=thunderc
-BOARD_KERNEL_BASE := 0x12200000
-BOARD_KERNEL_PAGESIZE := 2048
-
-include device/lge/thunderc-common/BoardConfigCommon.mk
+include device/lge/thunderc/BoardConfig.mk
 
 TARGET_OTA_ASSERT_DEVICE := thunderc,VS660,thunderc_VS660
--- device.mk
+++ device.mk
@@ -1,20 +1,18 @@
-PRODUCT_COPY_FILES += device/lge/thunderc-common/configs/media_profiles.xml:system/etc/media_profiles.xml
-
-# XXX: should be full_base_telephony?
-$(call inherit-product, build/target/product/full.mk)
-
-$(call inherit-product, build/target/product/languages_small.mk)
-$(call inherit-product, vendor/cm/config/common.mk)
 $(call inherit-product-if-exists, vendor/lge/VS660/VS660-vendor.mk)
+
+include device/lge/thunderc/device.mk
 
 CDMA_GOOGLE_BASE := android-verizon
 CDMA_CARRIER_ALPHA := Verizon_Wireless
 CDMA_CARRIER_NUMERIC := 310012
 
-include device/lge/thunderc-common/device.mk
-
-# Recent CM9 updates have pushed the recovery size beyond what the VS660
-# will ever be able to handle, so don't bother pretending...
-RECOVERY_REMOVE_FILES += res sbin
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase.ms=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.am=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.yt=$(CDMA_GOOGLE_BASE) \
+    ro.cdma.home.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    ro.cdma.home.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
+    gsm.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    gsm.operator.numeric=$(CDMA_CARRIER_NUMERIC)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/VS660/overlay
